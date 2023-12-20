8c5a689fa94e84cb8fd18ab8f0d00abe90f46810
Erick Smith <zmyth_eryck@hotmail.com>
2023-12-20 14:49:03 -0600

    actulualizacion dentro del archivo app,py

--- .streamlit/app.py
+++ .streamlit/app.py
@@ -3,3 +3,5 @@ import streamlit as st
 st.header('Lanzar una moneda')
 
 st.write('Esta aplicación aún no es funcional. En construcción.')
+
+.git .gitignore .streamlit app.py README.md requirements.txt

fe76f30483868ba6a2198ec74c8fbde76a1920e3
Erick Smith <zmyth_eryck@hotmail.com>
2023-12-21 13:48:25 -0600

    Se mueven las carpetas al lugar correcto

--- .streamlit/app.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-st.header('Lanzar una moneda')
-
-st.write('Esta aplicación aún no es funcional. En construcción.')
-
-$ ls -a
-
-.git .gitignore .streamlit app.py README.md requirements.txt
